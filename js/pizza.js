5795e7f993149cb67f57d4d907b8be601eada81f
Keegan18 <kolbeinsen.tafaoa@gmail.com>
2018-02-07 14:39:42 -1000

    Update 1

--- js/pizza.js
+++ js/pizza.js
@@ -4,7 +4,49 @@
 * Each object will have two nested objects ex.(pizza & toppings)
 * Keys with more than one value should be an array of strings ex. Veggies
 */
+var order11821 = {
+	customerName: "James Franko",
+	address: "2155 E Lake Dr",
+	city: "Seattle",
+	state: "WA",
+	zipCode: 11310,
+	pizza: {
+		crust: "thin",
+		pizzaSauce: "robust marinara",
+		cheese: "regular", 
+		toppings: {
+			veggies: ["olives", "green peppers"], 
+			meats: ["pepperoni", "ham", "sausage"]
+		}
+	},
+	sideItems: ["breadsticks", "bbq wings 6 piece"],
+	sauces: "ranch",
+	specialInstructions: "please cut into squares",
+	paymentType: "cash",
+	storeId: 112,
+}
 
+var order11372 = {
+	customerName: "Justin Bieber",
+	address: "1801 W Rodeo Dr",
+	city: "Beverly Hills",
+	state: "CA",
+	zipCode: 91833,
+	pizza: {
+		crust: "stuffed crust",
+		pizzaSauce: "creamy garlic alfredo",
+		cheese: "double",
+		toppings: {
+			veggies: ["onion", "anchovies", "mushrooms"],
+			meats: ["chicken", "bacon"]
+		}
+	},
+	sideItems: "double fudge brownie dessert",
+	sauces: ["bbq", "garlic sauce"],
+	specialInstructions: "call 815 @ the gate",
+	paymentType: "credit card",
+	storeId: 382,
+}
 
 /*
 * Create a new variable named currentOrders
