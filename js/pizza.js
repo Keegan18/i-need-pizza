ded8c6f82c1d1474607d5cd351b0a5f949edec2e
unknown <kolbeinsen.tafaoa@gmail.com>
2018-02-09 06:52:23 -1000

    Update FINAL

--- js/pizza.js
+++ js/pizza.js
@@ -24,7 +24,7 @@ var order11821 = {
 	specialInstructions: "please cut into squares",
 	paymentType: "cash",
 	storeId: 112,
-}
+};
 
 var order11372 = {
 	customerName: "Justin Bieber",
@@ -46,15 +46,65 @@ var order11372 = {
 	specialInstructions: "call 815 @ the gate",
 	paymentType: "credit card",
 	storeId: 382,
-}
+};
 
 /*
 * Create a new variable named currentOrders
 * currentOrders should be single object containing both orders from above
 */
+var currentOrders = {
+	order11821: {
+	customerName: "James Franko",
+	address: "2155 E Lake Dr",
+	city: "Seattle",
+	state: "WA",
+	zipCode: 11310,
+	pizza: {
+		crust: "thin",
+		pizzaSauce: "robust marinara",
+		cheese: "regular", 
+		toppings: {
+			veggies: ["olives", "green peppers"], 
+			meats: ["pepperoni", "ham", "sausage"]
+		}
+	},
+	sideItems: ["breadsticks", "bbq wings 6 piece"],
+	sauces: "ranch",
+	specialInstructions: "please cut into squares",
+	paymentType: "cash",
+	storeId: 112,
+	},
+	order11372 = {
+	customerName: "Justin Bieber",
+	address: "1801 W Rodeo Dr",
+	city: "Beverly Hills",
+	state: "CA",
+	zipCode: 91833,
+	pizza: {
+		crust: "stuffed crust",
+		pizzaSauce: "creamy garlic alfredo",
+		cheese: "double",
+		toppings: {
+			veggies: ["onion", "anchovies", "mushrooms"],
+			meats: ["chicken", "bacon"]
+		}
+	},
+	sideItems: "double fudge brownie dessert",
+	sauces: ["bbq", "garlic sauce"],
+	specialInstructions: "call 815 @ the gate",
+	paymentType: "credit card",
+	storeId: 382,
+};
 
 /*
 * Make the changes to the currentOrders object based on the ReadMe
 * Run your file with node pizza.js
 * Console.log(currentOrders) to verify changes have been made
 */
+
+currentOrders.order11821.Pizza["Pizza Sauce"] = "classic marinara";
+currentOrders.order11821.Pizza["Toppings"].Veggies = ["onion", "anchovies", "mushrooms", "Spinach"];
+currentOrders.order11821.Pizza["Toppings"].Meat = ["pepperoni", "ham"];
+currentOrders.order11821.orderTime = "20 minutes";
+
+console.log(currentOrders);
